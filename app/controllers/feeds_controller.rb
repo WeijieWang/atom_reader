37ddd8a2cbfba52d7ed4cf67a4fcc2257e5a23b9
Weijie <wwj0405@gmail.com>
2013-05-01 15:02:44 -0500

    subscription form

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,6 +1,21 @@
 class FeedsController < ApplicationController
   
   def index
-    
+    @feeds = Feed.all
   end
+  
+  def create
+    url = params[:feed][:url]
+    url = url.strip and url = url.gsub(/\A.*\/\//,"")
+    created_feed = Feedzirra::Feed.fetch_and_parse(url)
+
+    redirect_to :back and return if Feed.exists? url: created_feed.feed_url
+
+    @feed = Feed.new
+    @feed.url = created_feed.feed_url and @feed.title = created_feed.title
+    @feed.save
+
+    redirect_to :back
+  end
+  
 end
