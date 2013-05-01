735059fa89747d55db18c6d633aa07e912378b22
Weijie <wwj0405@gmail.com>
2013-05-01 14:22:07 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtomReader::Application.config.secret_token = 'c8bcfd1f4f9c1d392a613832501c210388005d94cd8bb767759d8b5ea8fe517550c5221eb37bd0e26f29100e3ee1bbf9775129f00a67f98d2425c8bb93311ce0'
