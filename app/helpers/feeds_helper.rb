f0cca5afe45b5b9713ffa56424a1fc28ac73e2a4
Weijie <wwj0405@gmail.com>
2013-05-11 12:39:58 -0500

    minor

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,12 +1,17 @@
 module FeedsHelper
-  
   def author_and_date entry
-    date = entry.published.strftime('at %I:%M %p %b %d, %Y ')
+
+    if entry.published.nil?
+      date = entry.created_at.strftime('at %I:%M %p %b %d, %Y ')
+    else
+      date = entry.published.strftime('at %I:%M %p %b %d, %Y ')
+    end
+
     if entry.author.nil?
       return date
     else
       return "by #{entry.author} "+ date
     end
   end
-    
+
 end
