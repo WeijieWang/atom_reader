5ef05c9dae0742d318e90ecaa6ec13a5b46943c9
Weijie <wwj0405@gmail.com>
2013-05-11 00:46:12 -0500

    next-button great work

--- app/assets/javascripts/feeds.js
+++ app/assets/javascripts/feeds.js
@@ -1,8 +1,26 @@
-$(document).ready(function() {
-	var pageAlert = function() {
-		alert("This is working");
-	};
+var pageRefresh = function() {
+	alert("This is under construction!");
+};
 
-	$('#refresh-button').on('click', pageAlert);
+var previousEntry = function() {
+	alert($('#right-part').scrollTop());
+};
+
+var nextEntry = function() {
+	var entry = $('.wrapped-entry').first(), margin = 20, 
+	distance = entry.height() + margin, d = entry.offset().top;
+	while (!((d <= 80) && (d > -(entry.height() - 80)))) {
+		entry = entry.next();
+		d = entry.offset().top;
+		distance += entry.height() + margin;
+	}
+	$('#right-part').scrollTop(distance);
+};
+
+$(document).ready(function() {
+
+	$('#refresh-button').on('click', pageRefresh);
+	$('#next-button').on('click', nextEntry);
+	$('#previous-button').on('click', previousEntry);
 
 });
